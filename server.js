bb0d6097ce23277629cdaac4f6963ca9fb3ecbed
root <root@ip-10-0-0-10.ap-south-1.compute.internal>
2019-06-03 12:31:56 +0000

    Third commit

--- server.js
+++ server.js
@@ -22,9 +22,8 @@ request.get("http://13.233.151.2:8000/", (err, res, body) => {
 
 //POST request withut using html page
 
-Var1 = "Mango"
 
-var data = '{ "request" : "msg", "data" : "India" }';
+var data = '{ "request" : "msg", "data" : "Mango" }';
 var json_obj = JSON.parse(data);
 request.post({
     headers: {'content-type': 'application/json'},
